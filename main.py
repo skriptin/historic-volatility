aac649ed6860f0d0cd094b00b7f70a92ea19ea8a
Liam <liamtheboss1@gmail.com>
2024-11-04 21:27:37 -0800

    Added a function and imput field, windowed_vol, this lets the user type in the window they want there lognormal volatility for there given stock to be calculated on, next steps are to apply exponential weighting this

--- main.py
+++ main.py
@@ -109,16 +109,49 @@ def calc_vol_daily(df):
             today_close = df.iloc[index]['Close']
             yesterday_close = df.iloc[index-1]['Close']
             volatility = abs(math.log(today_close/yesterday_close))
-            volatility = math.sqrt(volatility*TRADING_DAYS)*100 #we assume population statistic for n=1
+            volatility = math.sqrt(volatility)*100*math.sqrt(TRADING_DAYS) #we assume population statistic for n=1
             volatility = round(volatility, 2)
 
             daily_volatility[df.iloc[index]['Date']] = volatility
 
     return daily_volatility
 
+def calc_vol_windowed(df, window_length: int):
+    #Initalizations
+    windowed_volatility = {df['Date'].iloc[0].date(): 0}
+    TRADING_DAYS = 252
+    i = window_length
+    daily_returns = [0]*window_length
+
+    # Theese days will be without data
+    for index in range(0, window_length):
+        windowed_volatility[df.iloc[index]['Date']] = 0
+
+    while i < len(df):
+        median = 0
+        squared_differences = 0
+
+        for index in range(i-window_length, i):
+            median = median + math.log(df.iloc[index+1]['Close']/df.iloc[index]['Close'])
+            daily_returns[(index+1) % 5] = math.log(df.iloc[index+1]['Close']/df.iloc[index]['Close'])
+        median = median / window_length
+
+        for index in range(i-window_length, i):
+            squared_differences = squared_differences + (daily_returns[(index+1) % 5] - median) ** 2
+
+        windowed_vol = math.sqrt( (1/(window_length-1)) * squared_differences * 252 )*100
+        windowed_vol = round(windowed_vol,4)
+
+        windowed_volatility[df.iloc[i]['Date']] = windowed_vol
+        print("Vol at index  ",i,windowed_vol)
+
+        i += 1
+    return windowed_volatility
+
 def main():
     # Get user input
     ticker = get_user_input("Enter the ticker symbol: ").upper()
+    window_length = get_user_input("Please enter the window length used for the volatility calculations: ")
 
     # Validate start date
     while True:
@@ -134,7 +167,9 @@ def main():
             break
         print("Invalid date format. Please use YYYY-MM-DD.")
 
+
     print("Ticker:", ticker)
+    print("Window Length:",window_length)
     print("Start Date:", start_date)
     print("End Date:", end_date)
 
@@ -142,38 +177,42 @@ def main():
     df.reset_index(inplace=True)
 
 
-    daily_volatility = calc_vol_daily(df)
-    intra_day_volatility = calc_intra_day_vol(df)
-    weekly_vol = calc_weekly_vol(df)
+    #daily_volatility = calc_vol_daily(df)
+    #intra_day_volatility = calc_intra_day_vol(df)
+    #weekly_vol = calc_weekly_vol(df)
+    windowed_vol = calc_vol_windowed(df,int(window_length))
 
-    dates = list(daily_volatility.keys())
-    volatility = list(daily_volatility.values())
+    dates_w = list(windowed_vol.keys())
+    volatility_w = list(windowed_vol.values())
+    plt.plot(dates_w, volatility_w, label='f" Annualized {window_length} Volatility of {ticker} "', color='blue', linestyle='-', marker='o')
+    #dates = list(daily_volatility.keys())
+    #volatility = list(daily_volatility.values())
 
-    dates2 = list(intra_day_volatility.keys())
-    volatility2 = list(intra_day_volatility.values())
+    #dates2 = list(intra_day_volatility.keys())
+    #volatility2 = list(intra_day_volatility.values())
 
-    dates3 =list(weekly_vol.keys())
-    volatility3 = list(weekly_vol.values())
+    #dates3 =list(weekly_vol.keys())
+    #volatility3 = list(weekly_vol.values())
 
-    filtered_dates = [date for date, vol in zip(dates2, volatility2) if vol is not None]  # Only include if vol is not None
-    filtered_volatility = [vol for vol in volatility2 if vol is not None]  # Corresponding volatilities
+    #filtered_dates = [date for date, vol in zip(dates2, volatility2) if vol is not None]  # Only include if vol is not None
+    #filtered_volatility = [vol for vol in volatility2 if vol is not None]  # Corresponding volatilities
 
-    filtered_dates2 = [date for date, vol in zip(dates, volatility) if vol is not None]  # Only include if vol is not None
-    filtered_volatility2 = [vol for vol in volatility if vol is not None]  # Corresponding volatilities
+    #filtered_dates2 = [date for date, vol in zip(dates, volatility) if vol is not None]  # Only include if vol is not None
+    #filtered_volatility2 = [vol for vol in volatility if vol is not None]  # Corresponding volatilities
 
-    filtered_dates3 = [date for date, vol in zip(dates3, volatility3) if vol is not None]  # Only include if vol is not None
-    filtered_volatility3 = [vol for vol in volatility3 if vol is not None]  # Corresponding volatilities
+    #filtered_dates3 = [date for date, vol in zip(dates3, volatility3) if vol is not None]  # Only include if vol is not None
+    #filtered_volatility3 = [vol for vol in volatility3 if vol is not None]  # Corresponding volatilities
 
     # Create a line graph
-    plt.plot(filtered_dates2, filtered_volatility2, label='Interday vol(d1open-d2open)', color='blue', linestyle='-', marker='o')
-    plt.plot(filtered_dates, filtered_volatility, label='Intraday Vol (high-low)', color='red', linestyle='--', marker='x')
-    plt.plot(filtered_dates3, filtered_volatility3, label='Intraweekly Vol', color='Green', linestyle='--', marker='o')
+    #plt.plot(filtered_dates2, filtered_volatility2, label='Interday vol(d1open-d2open)', color='blue', linestyle='-', marker='o')
+    #plt.plot(filtered_dates, filtered_volatility, label='Intraday Vol (high-low)', color='red', linestyle='--', marker='x')
+    #plt.plot(filtered_dates3, filtered_volatility3, label='Intraweekly Vol', color='Green', linestyle='--', marker='o')
 
 
 
     # Add titles and labels
-    plt.title('ticker')
-    plt.xlabel('Date')
+    plt.title(ticker)
+    plt.xlabel('Dates')
     plt.ylabel('Volatility')
 
     # Rotate x-axis labels for better readability
