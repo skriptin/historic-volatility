bd8508880cce8935d88a1bf4921efd1112579b20
Liam <liamtheboss1@gmail.com>
2024-11-07 21:51:27 -0800

    Fully implemented EWMA historic volatility

--- main.py
+++ main.py
@@ -129,19 +129,17 @@ def calc_vol_EWMA(df, window_length: int, lam_bda: float):
     for index in range(1,window_length+1):
         exp_ma[df.iloc[index]['Date']] = 0
         squared_return = math.log(df.iloc[index]['Close']/df.iloc[index-1]['Close']) ** 2
-        last_var = last_var + ((squared_return * alpha) * (lam_bda ** window_length - index))
+        last_var = last_var + ((squared_return * alpha) * (lam_bda ** (window_length - index)))
 
-    exp_ma[df.iloc[window_length+1]['Date']] = math.root(inital_var)*100
+    print("Volatility for the 6th point ",math.sqrt(last_var*252)*100)
+    exp_ma[df.iloc[window_length+1]['Date']] = math.sqrt(last_var)*100
     i = window_length+2
-    while i <= len(df):
-
-
-        volatility = lam_bda*last_var + (math.log(df.iloc[i-1]['Close']/df.iloc[i-2]['Close']) ** 2)
+    while i < len(df):
+        squared_return = math.log(df.iloc[i-1]['Close']/df.iloc[i-2]['Close']) ** 2
+        volatility = (lam_bda*last_var) + (squared_return*alpha) 
         last_var = volatility 
-        exp_ma[df.iloc[i]['Date']] = math.sqrt(volatility)
-
-
-
+        exp_ma[df.iloc[i]['Date']] = math.sqrt(volatility*252) * 100
+        i += 1
     return exp_ma
 
 
@@ -154,49 +152,54 @@ def calc_vol_windowed(df, window_length: int):
     daily_returns = [0]*window_length
 
     # Theese days will be without data
-    for index in range(0, window_length):
+    for index in range(0, window_length+1):
         windowed_volatility[df.iloc[index]['Date']] = 0
 
-    while i < len(df):
+    while i < len(df) - 1:
         median = 0
         squared_differences = 0
 
-        for index in range(i-window_length, i):
-            median = median + math.log(df.iloc[index+1]['Close']/df.iloc[index]['Close'])
-            daily_returns[(index+1) % window_length] = math.log(df.iloc[index+1]['Close']/df.iloc[index]['Close'])
+        for index in range(i-window_length + 1, i + 1):
+            median = median + math.log(df.iloc[index]['Close']/df.iloc[index-1]['Close'])
+            daily_returns[(index) % window_length] = math.log(df.iloc[index]['Close']/df.iloc[index-1]['Close'])
         median = median / window_length
 
-        for index in range(i-window_length, i):
-            squared_differences = squared_differences + (daily_returns[(index+1) % window_length] - median) ** 2
+        for index in range(i-window_length+1, i + 1):
+            squared_differences = squared_differences + (daily_returns[(index) % window_length] - median) ** 2
 
-        windowed_vol = math.sqrt( (1/(window_length-1)) * squared_differences * 252 )*100
+        windowed_vol = math.sqrt( (1/(window_length-1)) * squared_differences * 252 )* 100
         windowed_vol = round(windowed_vol,4)
 
-        windowed_volatility[df.iloc[i]['Date']] = windowed_vol
-        print("Vol at index  ",i,windowed_vol)
+        windowed_volatility[df.iloc[i+1]['Date']] = windowed_vol
+        #print("Vol at index  ",i+1,windowed_vol)
 
         i += 1
     return windowed_volatility
 
 def main():
     # Get user input
-    ticker = get_user_input("Enter the ticker symbol: ").upper()
-    window_length = get_user_input("Please enter the window length used for the volatility calculation: ")
-    lam_bda = get_user_input("Please enter lambda for the weight of the EWMA volatility calculation: ")
+    #ticker = get_user_input("Enter the ticker symbol: ").upper()
+    #window_length = get_user_input("Please enter the window length used for the volatility calculation: ")
+    #lam_bda = get_user_input("Please enter lambda for the weight of the EWMA volatility calculation: ")
     # Validate start date
-    while True:
-        start_date = get_user_input("Enter the starting date (YYYY-MM-DD): ")
-        if validate_date(start_date):
-            break
-        print("Invalid date format. Please use YYYY-MM-DD.")
+    #while True:
+    #    start_date = get_user_input("Enter the starting date (YYYY-MM-DD): ")
+    #    if validate_date(start_date):
+    #        break
+   #     print("Invalid date format. Please use YYYY-MM-DD.")
 
     # Validate end date
-    while True:
-        end_date = get_user_input("Enter the ending date (YYYY-MM-DD): ")
-        if validate_date(end_date):
-            break
-        print("Invalid date format. Please use YYYY-MM-DD.")
+   # while True:
+   #     end_date = get_user_input("Enter the ending date (YYYY-MM-DD): ")
+    #    if validate_date(end_date):
+    #        break
+    #    print("Invalid date format. Please use YYYY-MM-DD.")
 
+    ticker = "SPY"
+    window_length = 50
+    start_date = "2001-01-01"
+    end_date = "2024-11-01"
+    lam_bda = 0.9
 
     print("Ticker:", ticker)
     print("Window Length:",window_length)
@@ -207,41 +210,21 @@ def main():
     df.reset_index(inplace=True)
 
 
-    #daily_volatility = calc_vol_daily(df)
-    #intra_day_volatility = calc_intra_day_vol(df)
-    #weekly_vol = calc_weekly_vol(df)
     windowed_vol = calc_vol_windowed(df,int(window_length))
+    window_length = 10
     EWMA_vol = calc_vol_EWMA(df, int(window_length), float(lam_bda))
 
 
 
     dates_w = list(windowed_vol.keys())
     volatility_w = list(windowed_vol.values())
+
+    dates_e = list(EWMA_vol.keys())
+    volatility_e = list(EWMA_vol.values())
+
+
     plt.plot(dates_w, volatility_w, label='f" Annualized {window_length} Volatility of {ticker} "', color='blue', linestyle='-', marker='o')
-    #dates = list(daily_volatility.keys())
-    #volatility = list(daily_volatility.values())
-
-    #dates2 = list(intra_day_volatility.keys())
-    #volatility2 = list(intra_day_volatility.values())
-
-    #dates3 =list(weekly_vol.keys())
-    #volatility3 = list(weekly_vol.values())
-
-    #filtered_dates = [date for date, vol in zip(dates2, volatility2) if vol is not None]  # Only include if vol is not None
-    #filtered_volatility = [vol for vol in volatility2 if vol is not None]  # Corresponding volatilities
-
-    #filtered_dates2 = [date for date, vol in zip(dates, volatility) if vol is not None]  # Only include if vol is not None
-    #filtered_volatility2 = [vol for vol in volatility if vol is not None]  # Corresponding volatilities
-
-    #filtered_dates3 = [date for date, vol in zip(dates3, volatility3) if vol is not None]  # Only include if vol is not None
-    #filtered_volatility3 = [vol for vol in volatility3 if vol is not None]  # Corresponding volatilities
-
-    # Create a line graph
-    #plt.plot(filtered_dates2, filtered_volatility2, label='Interday vol(d1open-d2open)', color='blue', linestyle='-', marker='o')
-    #plt.plot(filtered_dates, filtered_volatility, label='Intraday Vol (high-low)', color='red', linestyle='--', marker='x')
-    #plt.plot(filtered_dates3, filtered_volatility3, label='Intraweekly Vol', color='Green', linestyle='--', marker='o')
-
-
+    plt.plot(dates_e, volatility_e, label='f" Annualized {window_length} Volatility of {ticker} WITH EMA "', color='red', linestyle='-', marker='x')
 
     # Add titles and labels
     plt.title(ticker)
@@ -257,45 +240,6 @@ def main():
     plt.show()
 
 
-def tester():
-    df = yf.download('SPY', start="2024-01-01", end="2024-10-01")
-    #print(df)
-    try:
-        df.to_csv('rawdata.csv')
-    except PermissionError:
-        print("Error: Permission denied")
-    except Exception as e:
-        print(f"Error occured {e}")
-    daily_volatility = calc_vol_daily(df)
-    intra_day_volatility = calc_intra_day_vol(df)
-
-    dates = list(daily_volatility.keys())
-    volatility = list(daily_volatility.values())
-
-    dates1 = list(intra_day_volatility.keys())
-    volatility2 = list(intra_day_volatility.values())
-
-    # Create a line graph
-    plt.plot(dates, volatility, label='Dataset A', color='blue', linestyle='-', marker='o')
-    plt.plot(dates1, volatility2, label='Dataset B', color='red', linestyle='--', marker='x')
-
-    # Add titles and labels
-    plt.title('SPY')
-    plt.xlabel('Date')
-    plt.ylabel('Volatility')
-
-    # Rotate x-axis labels for better readability
-    plt.xticks(rotation=45)
-    plt.legend()
-
-    # Show the plot
-    plt.tight_layout()  # Adjust layout to prevent clipping of tick-labels
-    plt.show()
-
-
-
-
 
 if __name__ == "__main__":
     main()
-    #tester()
