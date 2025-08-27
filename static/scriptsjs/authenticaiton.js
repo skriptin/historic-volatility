996851ffc47bb6637632fad04507df98fe3e1968
Liam <liamtheboss1@gmail.com>
2025-08-26 20:20:29 -0700

    fire base api

--- static/scriptsjs/authenticaiton.js
+++ static/scriptsjs/authenticaiton.js
@@ -1,9 +1,18 @@
-  const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-  };
+  
+  
+const firebaseConfig = {
+apiKey: "AIzaSyAdIFpfDS5gFbxMgZJ2cKNWh1QTXrZae2U",
+authDomain: "volatility-models-67019.firebaseapp.com",
+projectId: "volatility-models-67019",
+storageBucket: "volatility-models-67019.firebasestorage.app",
+messagingSenderId: "190771080240",
+appId: "1:190771080240:web:0295317bcdd7a20475d1f5",
+measurementId: "G-5Y3P1JY245"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
+
+
+
+const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
